2aaa25f05111d0ffe36bfed3ca29d4c3cfed6622
David Wolever <david@wolever.net>
2013-06-16 19:41:36 -0400

    Adding 'open_atomic'

--- test/test_standard.py
+++ test/test_standard.py
@@ -1,11 +1,15 @@
+import io
+import os
 import sys
 import unittest
+import tempfile
 
 
 sys.path.append('../')
 
 
 from unstdlib.standard.collections_ import RecentlyUsedContainer
+from unstdlib.standard.contextlib_ import open_atomic
 
 
 class TestRecentlyUsedContainer(unittest.TestCase):
@@ -26,6 +30,84 @@ class TestRecentlyUsedContainer(unittest.TestCase):
         self.assertFalse(0 in d)
         self.assertTrue(i+1 in d)
 
+
+class TestOpenAtomic(unittest.TestCase):
+    class ExpectedException(Exception):
+        pass
+
+    def setUp(self):
+        self.testfile = os.path.join(tempfile.gettempdir(),
+                                     "unstdlib-atmoicopen-test")
+        self.rmtestfile()
+
+    def tearDown(self):
+        self.rmtestfile()
+
+    def rmtestfile(self):
+        try:
+            os.remove(self.testfile)
+        except OSError:
+            pass
+
+    def test_context_manager_success(self):
+        with open_atomic(self.testfile) as f:
+            f.write("Hello, world!")
+            self.assertFalse(os.path.exists(self.testfile))
+            self.assertEqual(os.path.dirname(self.testfile),
+                             os.path.dirname(f.temp_name))
+        self.assertTrue(os.path.exists(self.testfile))
+        self.assertEqual(open(self.testfile).read(), "Hello, world!")
+
+    def test_context_manager_fail(self):
+        temp_name = None
+        try:
+            with open_atomic(self.testfile) as f:
+                f.write("Hello, world!")
+                temp_name = f.temp_name
+                self.assertTrue(os.path.exists(temp_name))
+                raise self.ExpectedException
+        except self.ExpectedException:
+            pass
+        self.assertFalse(os.path.exists(self.testfile))
+        self.assertFalse(os.path.exists(temp_name))
+
+    def test_abort(self):
+        with open_atomic(self.testfile) as f:
+            self.assertTrue(os.path.exists(f.temp_name))
+            f.abort()
+            self.assertEqual(f.abort_error, None)
+            self.assertTrue(f.aborted)
+            self.assertTrue(f.closed)
+            self.assertFalse(os.path.exists(self.testfile))
+            self.assertFalse(os.path.exists(f.temp_name))
+
+    def test_abort_fails(self):
+        with open_atomic(self.testfile) as f:
+            os.remove(f.temp_name)
+            f.abort()
+            self.assertIn("No such file or directory", str(f.abort_error))
+            self.assertTrue(f.aborted)
+            self.assertTrue(f.closed)
+
+    def test_close_fails(self):
+        with open_atomic(self.testfile) as f:
+            os.remove(f.temp_name)
+            try:
+                f.close()
+            except OSError as e:
+                self.assertIn("No such file or directory", str(e))
+            self.assertIn("No such file or directory", str(f.abort_error))
+            self.assertTrue(f.aborted)
+            self.assertTrue(f.closed)
+
+    def test_file_operations(self):
+        with open_atomic(self.testfile, open_func=io.open,
+                        encoding="utf-8", mode="w+") as f:
+            f.write(u"\u1234")
+            f.seek(0)
+            self.assertEqual(f.tell(), 0)
+            x = f.read()
+            self.assertEqual(x, u"\u1234")
 
 if __name__ == '__main__':
     unittest.main()
